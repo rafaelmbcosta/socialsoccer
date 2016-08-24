1590d3cdc0b6d7db2001b6ecaf75ac51342016e0
Rafael Costa <rafaelmbcosta@gmail.com>
2016-08-24 08:11:39 -0300

    top strikers json

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,14 @@
 class ReportsController < ApplicationController
 	def top_strikers
 		@season = Season.last
+	    @goals = @season.goals
+		@matches = @season.matches.size
 		@result = @season.top_strikers
+		json_result = {id: @season.id, goals: @goals, matches: @matches, players: @result}
+		respond_to do |format|
+			format.html
+			format.json { render json: json_result }
+		end
 	end
 
 	def matches
