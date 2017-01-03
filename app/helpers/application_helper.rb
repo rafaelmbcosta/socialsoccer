d87dd3ed083a93abb9e0cc4785ba07a139db2a02
Rafael Costa <rafaelmbcosta@gmail.com>
2017-01-03 16:43:11 -0300

    Admin menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
 		name += " (#{player.nickname})" unless (player.nickname.nil? or player.nickname.blank?)
 		return name
 	end
+
+	def open_matches
+		return Match.unfinished.size
+	end
 end
