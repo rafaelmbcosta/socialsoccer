f6903ce180e76b87f0bf468687cbdb567a1ae66d
Rafael Costa <rafaelmbcosta@gmail.com>
2017-02-10 18:22:58 -0300

    Adding default values for description

--- db/migrate/20170210011611_add_match_id_and_date_and_description_to_videos.rb
+++ db/migrate/20170210011611_add_match_id_and_date_and_description_to_videos.rb
@@ -2,6 +2,6 @@ class AddMatchIdAndDateAndDescriptionToVideos < ActiveRecord::Migration
   def change
     add_column :videos, :match_id, :integer, default: nil
     add_column :videos, :date, :date, null: false, default: ( Time.now - 1.year )
-    add_column :videos, :description, :string, null: false
+    add_column :videos, :description, :string, null: false, default: "Social Video"
   end
 end
