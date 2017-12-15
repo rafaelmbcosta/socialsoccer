e5c7f70028fd72e2e3fb2e1eac041580728ac545
Rafael Costa <rafaelmbcosta@gmail.com>
2017-12-14 23:08:34 -0300

    layout improvements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require tether
+//= require jquery3
+//= require popper
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
